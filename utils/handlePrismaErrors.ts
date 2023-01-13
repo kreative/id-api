e54af95969064f7f504ae61a6d1c7d4240eb62ca
Armaan Gupta <armaan@guppy.im>
2023-01-13 14:40:49 -0600

    fix: added error handling for P2003

--- utils/handlePrismaErrors.ts
+++ utils/handlePrismaErrors.ts
@@ -17,13 +17,16 @@ export function handlePrismaErrors(error: any): void {
   
   // handles errors of PrismaClientKnownRequestError
   if (error instanceof PrismaClientKnownRequestError) {
+    console.log(error);
+
     if (error.code === "P2002") {
-      console.log(error);
       throw new ForbiddenException();
+    }
+    else if (error.code === "P2003") {
+      throw new BadRequestException("failed foreign key constraint");
     }
     else {
       // responds to all other errors as a 'catch-all'
-      console.log(error);
       throw new InternalServerErrorException()
     }
   }
