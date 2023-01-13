1834c5fd720170e2b176c7934a49815324a2533d
Armaan Gupta <armaan@guppy.im>
2023-01-13 13:32:41 -0600

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('/v1');
 
