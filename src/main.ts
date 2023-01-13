2f57b3e84af7f6b4a048617908c9a3eff5310714
Armaan Gupta <armaan@guppy.im>
2023-01-12 22:21:11 -0600

    feat: added changes to entry file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('/api/v1');
+
   await app.listen(3000);
 }
+
 bootstrap();
