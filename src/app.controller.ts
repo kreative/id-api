2922321e7532d14fb4772d884335f0f7837973b6
Armaan Gupta <armaan@guppy.im>
2023-01-17 09:51:56 -0600

    test: added sample error route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 
 @Controller('')
 export class AppController {
   @Get()
   async home(): Promise<string> {
     return "Welcome to the good place, friend. It's good to see you.";
+  };
+  
+  @Get('throw')
+  throwError(): string {
+    throw new HttpException({ message: 'Sample Error' }, 500);
   }
 }
